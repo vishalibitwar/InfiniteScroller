e27e05fce8f4d7c754921980b5ef21dec0f4cf54
vishalibitwar <vish31ibitwar@gmail.com>
2019-12-10 02:26:52 +0530

    Files Added

---
+++ js/app.js
@@ -0,0 +1,54 @@
+const container = document.getElementById('container');
+const loading = document.querySelector('.loading');
+
+newUserProfile();
+newUserProfile();
+newUserProfile();
+newUserProfile();
+
+
+window.addEventListener('scroll', () => {
+  const {
+    scrollTop,
+    scrollHeight,
+    clientHeight
+  } = document.documentElement;
+  if (scrollTop + clientHeight >= scrollHeight - 5) {
+    showLoading();
+  }
+});
+
+function showLoading() {
+  loading.classList.add('show');
+  setTimeout(() => {
+    loading.classList.remove('show');
+    setTimeout(() => {
+      newUserProfile();
+    }, 300);
+  }, 1000);
+}
+
+function newUserProfile() {
+  fetch(`https://randomuser.me/api/?results=1&inc=name,location,email,phone,dob,picture&nat=us,dk,au,nz,fr,ca,ch,br,de,es,fi,gb,ie,no,nl`)
+    .then(res => res.json())
+    .then(data => {
+      showprofile(data.results[0]);
+    })
+    .catch(error => console.log(error));
+  }
+
+function showprofile(data) {
+  const user = document.createElement('div');
+  user.classList.add('user');
+  user.innerHTML = `  
+		<img src="${data.picture.large}" alt="" />
+			<div class="user-info">
+      <h1 class="user-name">${data.name.first} ${data.name.last}</h1>
+			<p class="user-email">🌐   ${data.location.country} </p>      
+			<small class="user-dob">🎂 ${(data.dob.date).slice(0,10)}</small>
+			<p class="user-email">✉️   ${data.email} </p>
+			<p class="user-email">📞   ${data.phone} </p>
+    </div>    
+	`;
+  container.appendChild(user);
+}
