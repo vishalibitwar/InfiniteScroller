af7087169e3649691940be7922b47ec0307722cc
vishal ibitwar <vish31ibitwar@gmail.com>
2023-12-04 23:15:58 +0530

    Use IntersectObserver

--- js/app.js
+++ js/app.js
@@ -1,72 +1,59 @@
-const container = document.getElementById('container');
-const loading = document.querySelector('.loading');
-
+const container = document.getElementById("container");
+const loading = document.querySelector(".loading");
+let isLoading;
 newUserProfile();
 newUserProfile();
-newUserProfile();
-newUserProfile();
 
-
-
-
-// document.body.addEventListener( "touchstart", function(e){ onStart(e); }, false );
-// function onStart ( touchEvent ) {
-//   if( navigator.userAgent.match(/Android/i) ) {
-//     touchEvent.preventDefault();
-//   }
-// }
-function fixTouchMove( event )
-{
-    return;
-}
-document.body.removeEventListener( "touchstart", fixTouchMove );
-document.body.addEventListener( "touchstart", fixTouchMove );
-
-document.body.addEventListener('touchmove', onScroll);
-window.addEventListener('scroll', onScroll);
-
-
- function onScroll(){
-  const {
-    scrollTop,
-    scrollHeight,
-    clientHeight
-  } = document.documentElement;
-  if (scrollTop + clientHeight >= scrollHeight - 5) {
-    showLoading();
-}}
+const lastUserSelector = document.querySelector(".user:last-child");
 
 function showLoading() {
-  loading.classList.add('show');
+  if (isLoading) return false;
   setTimeout(() => {
-    loading.classList.remove('show');
-    setTimeout(() => {
-      newUserProfile();
-    }, 300);
-  }, 1000);
+    newUserProfile();
+    isLoading = false;
+    loading.classList.remove("show");
+  }, 200);
+
+  loading.classList.add("show");
+  isLoading = true;
 }
 
 function newUserProfile() {
-  fetch(`https://randomuser.me/api/?results=1&inc=name,location,email,phone,dob,picture&nat=us,dk,au,nz,fr,ca,ch,br,de,es,fi,gb,ie,no,nl`)
-    .then(res => res.json())
-    .then(data => {
-      showprofile(data.results[0]);
+  fetch(
+    `https://randomuser.me/api/?results=3&inc=name,location,email,phone,dob,picture&nat=us,dk,au,nz,fr,ca,ch,br,de,es,fi,gb,ie,no,nl`
+  )
+    .then((res) => res.json())
+    .then((data) => {
+      showprofile(data.results);
     })
-    .catch(error => console.log(error));
-  }
+    .catch((error) => console.log(error));
+}
 
-function showprofile(data) {
-  const user = document.createElement('div');
-  user.classList.add('user');
-  user.innerHTML = `  
+function showprofile(users) {
+  users.forEach((data) => {
+    const user = document.createElement("div");
+    user.classList.add("user");
+    user.innerHTML = `  
 		<img src="${data.picture.large}" alt="" />
 			<div class="user-info">
       <h1 class="user-name">${data.name.first} ${data.name.last}</h1>
 			<p class="user-email">🌐   ${data.location.country} </p>      
-			<small class="user-dob">🎂 ${(data.dob.date).slice(0,10)}</small>
+			<small class="user-dob">🎂 ${data.dob.date.slice(0, 10)}</small>
 			<p class="user-email">✉️   ${data.email} </p>
 			<p class="user-email">📞   ${data.phone} </p>
     </div>    
 	`;
-  container.appendChild(user);
+    container.appendChild(user);
+  });
 }
+const observer = new IntersectionObserver((entries) => {
+  const lastUser = entries[0];
+  if (!lastUser.isIntersecting) return;
+  showLoading();
+  observer.unobserve(lastUser.target);
+  observer.observe(document.querySelector(".user:last-child"));
+}, {});
+
+setTimeout(() => {
+  observer.observe(document.querySelector(".user:last-child"));
+}, 1000);
